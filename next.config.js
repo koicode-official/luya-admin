10fc2bd7e06e908d78fc0b3b2f3f75b7ad86c777
koicode-official <koicode@koicode.co.kr>
2023-10-04 15:20:59 +0900

    config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   basePath: '/admin', // 원하는 기본 경로를 여기에 설정합니다.
   assetPrefix: process.env.NEXT_PUBLIC_NODE_ENV === "production" ? "/admin/" : undefined,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'luya.co.kr',
+        port: '',
+        pathname: '/admin/**',
+      },
+    ],
+  },
 }
 
 
